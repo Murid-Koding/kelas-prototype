e24d3172400502cae7b03c602b176b00ea07723e
Tommy Alhamra <tommy.alhamra@gmail.com>
2020-12-01 17:12:21 +0700

    Refactoring Code

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https: //firebase.google.com/docs/web/setup#available-libraries
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD30PsnpQHAzlslAuMIVTXqcXwKd8L9vkM",
+  authDomain: "kelas-prototype.firebaseapp.com",
+  databaseURL: "https://kelas-prototype.firebaseio.com",
+  projectId: "kelas-prototype",
+  storageBucket: "kelas-prototype.appspot.com",
+  messagingSenderId: "1053238369081",
+  appId: "1:1053238369081:web:dd5dd9010ecadbfea666e8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
